7d97f9db826b7fb612b5f632e14ff02c2221baf3
anzhelaAbitova <55100446+anzhelaAbitova@users.noreply.github.com>
2021-08-01 23:13:04 +0400

    fix: components link

--- src/containers/table.js
+++ src/containers/table.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react'
 import { connect } from 'react-redux'
 import * as categoriesActions from '../store/categories/actions'
 import * as categoriesSelectors from '../store/categories/reducer'
-import Product from '../components/Product'
+import Product from './../components/Product'
 import Products from './Products'
 
 const Table = (props) => {
